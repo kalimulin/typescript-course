6871a40f6afb90ee8c3815b1cf99d3cb602a2226
Rustam Kalimulin <r.kalimulin@notamedia.ru>
2022-08-28 23:50:47 +0400

    система типов

---
+++ features/annotations/variables.ts
@@ -0,0 +1,10 @@
+let oranges: number = 5;
+let speed: string = 'fast';
+
+let hasDog: boolean = true;
+
+let nothing: null = null;
+let nothing1: undefined = undefined;
+
+// bult-in objects
+let now: Date = new Date();
