4acb658551f7a25eb1d74600cdd2f0ceec1a8f48
Rustam Kalimulin <r.kalimulin@notamedia.ru>
2022-08-29 19:42:12 +0400

    function annotations

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -1,3 +1,11 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+function multiply (a: number, b: number): number {
+  return a * b;
+}
